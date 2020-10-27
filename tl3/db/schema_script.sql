ecfee01c39ca97ce682828a491fe127a43632313
Pedro Konstantinoff <pkonstantinoff@gmail.com>
2020-10-27 20:22:45 -0300

    add dockerized cgi with postgres

---
+++ tl3/db/schema_script.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE users(
+    id serial PRIMARY KEY, 
+    name character(70) NOT NULL,
+    username character(70) NOT NULL,
+    age integer NOT NULL, 
+    password character(20) NOT NULL
+);
+
+INSERT INTO users (name, age, username, password) 
+  VALUES ('Pedro Konstantinoff',36,'pedro','un_password'); 
+
+SELECT * FROM users;
---
+++ tl3/src/backend/users.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+import os
+import cgi
+import cgitb
+import json
+import sqlalchemy
+from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+cgitb.enable()
+
+print("Content-Type: application/json;charset=utf-8")
+print()
+
+db_string = "postgres://admin:admin@tl3_db:5432/tl3"
+engine = create_engine(db_string, echo=False)
+
+metadata = MetaData()
+
+users_table = Table('users', metadata,
+     Column('id', Integer, primary_key=True),
+     Column('name', String),
+     Column('username', String),
+     Column('age', Integer),
+     Column('password', String),
+)
+
+metadata.create_all(engine) 
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+class User(declarative_base()):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    username = Column(String)
+    age = Column(Integer)
+    password = Column(String)
+
+    def __init__(self, name, age, username, password):
+        self.name = name
+        self.age = age
+        self.username = username
+        self.password = password
+
+
+def query_users():
+    users = []
+    for u in session.query(User).all():
+        user = u.__dict__
+        user.pop('_sa_instance_state', None)    
+        users.append(user)
+    return users
+
+
+def create_user():
+    try:
+        form = cgi.FieldStorage()
+        user = User(
+            form.getvalue('name'),
+            form.getvalue('age'),
+            form.getvalue('username'),
+            form.getvalue('password')
+            )
+        session.add(user)
+        session.commit()
+        return {'error': False}
+    except:
+        return {'error': True}
+    
+
+if os.environ['REQUEST_METHOD'] == 'GET':
+    response = query_users()    
+if os.environ['REQUEST_METHOD'] == 'POST':
+    response = create_user()
+if not response:
+    response = {}
+
+
+print(json.JSONEncoder().encode(response))

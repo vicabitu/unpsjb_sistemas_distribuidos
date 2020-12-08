49b6552d47d14823414e8e29467629102b40a08b
Pedro Konstantinoff <pkonstantinoff@gmail.com>
2020-12-08 12:29:51 -0300

    Some flask code

--- tl3/microservices/src/login/login.py
+++ tl3/microservices/src/login/login.py
@@ -4,33 +4,74 @@ from flask import (
     make_response, 
     jsonify, 
     request,
-) 
+)
+from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
+from db_handler import Database
+
+"""
+Flask related definitions
+"""
 app = Flask(__name__)
 
 PORT = 5000
 HOST = '0.0.0.0'
 
-json = {
-    "languajes": {
-        "es": "Spanish",
-        "en": "English",
-        "fr": "French",
-    },
-    "colors": {
-        "r": "Red",
-        "g": "Green",
-        "b": "Blue",
-   }
-}
+db = Database.instance()
 
-@app.route("/")
+
+@app.route("/", methods = ['POST', 'GET'])
 def index():
-    return "HELLO WORLD"
+
+    if request.method == 'GET':
+        print('INDEX || GET ')
+        print('INDEX || GET || test user query')
+        print(get_users())
+        return render_template("login.html")
+
+    if request.method == 'POST':
+        print('INDEX || POST ')
+        print(request)
+        user = request.form['username']
+        password = request.form['password']
+        # resp = make_response(render_template('login.html', result = result))
+        resp = make_response(render_template('login.html'))
+        resp.set_cookie('userID', user)
+        resp.set_cookie('userPASS', password)
+
+        return resp
+
+
+@app.route('/setcookie', methods = ['POST', 'GET'])
+def setcookie():
+    if request.method == 'POST':
+        user = request.form['nm']
+   
+    resp = make_response(render_template('readcookie.html'))
+    resp.set_cookie('userID', user)
+    
+    return resp
+
+
+@app.route('/getcookie')
+def getcookie():
+   name = request.cookies.get('userID')
+   return '<h1>welcome ' + name + '</h1>'
+
 
 @app.route("/template")
 def hola():
     return render_template("login.html")
+
+
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = request.form
+      return render_template("result.html",result = result)
+
 
 @app.route("/json")
 def get_json():
@@ -38,10 +79,12 @@ def get_json():
     data = {"user": "pedro"}
     return make_response(jsonify(data), 200)
 
+
 @app.route("/users/<user>")
 def get_user(user):    
     data = {"user": user}
     return make_response(jsonify(data), 200)
+
 
 @app.route("/users", methods=["POST"])
 def post_user():    
@@ -52,6 +95,7 @@ def post_user():
     req = request.get_json()
     return make_response(jsonify(req), 200)   
 
+
 if __name__ == '__main__':
     print(f"Login microservice runnning in port {PORT}")
-    app.run(host=HOST, port=PORT)
+    app.run(host=HOST, port=PORT, debug=True)

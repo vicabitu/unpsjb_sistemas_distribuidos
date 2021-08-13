73423a837ec7829f4c163b4686637792b4f1ba64
vicabitu <victor.ch370@gmail.com>
2021-08-13 19:41:30 -0300

    Add server stub

--- tl1/p1/server.py
+++ tl1/p1/server.py
@@ -1,17 +1,16 @@
 class Server:
-
     def __init__(self, adapter=None):
         self.adapter = adapter
 
     def config(self):
         self.adapter.config()
-    
+
     def start(self):
         self.adapter.start()
 
     def accept_connection(self):
         self.adapter.accept_connection()
-    
+
     def close_connection(self):
         self.adapter.close_connection()
 

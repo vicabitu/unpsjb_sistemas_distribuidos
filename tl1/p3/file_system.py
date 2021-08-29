b62393b16b0de51e4176bc0498ec3cbbb68dfa20
vicabitu <victor.ch370@gmail.com>
2021-08-29 00:09:46 -0300

    Several fixes in file system module

--- tl1/p3/file_system.py
+++ tl1/p3/file_system.py
@@ -1,42 +1,44 @@
 import os
 
-class FS:
-    file_manager = {}
 
-    def list_files(path):
+class FS:
+    def __init__(self):
+        self._file_manager = {}
+
+    def list_files(self, path):
         try:
             return os.listdir(path)
         except Exception as e:
-            print('ERROR!!! ', e)
+            print("ERROR!!! ", e)
             return None
 
-    def open_file(path):
+    def open_file(self, path):
         try:
-            if path not in file_manager:
-                _file = open(path, 'r')
-                file_manager[path] = _file
+            if path not in self._file_manager:
+                _file = open(path, "r")
+                self._file_manager[path] = _file
             return True
         except Exception as e:
-            print('ERROR!!! ', e)
+            print("ERROR!!! ", e)
             return False
 
-    def close_file(path):
+    def close_file(self, path):
         try:
-            if path in file_manager:
-                file_manager[path].close()
-                del file_manager[path]
+            if path in self._file_manager:
+                self._file_manager[path].close()
+                del self._file_manager[path]
             return True
         except Exception as e:
-            print('ERROR!!! ', e)
+            print("ERROR!!! ", e)
             return False
 
-    def read_file(path):
+    def read_file(self, path):
         try:
-            if open_file(path):
-                if path in file_manager:
-                    data = file_manager[path].read()
-                close_file(path)
+            if self.open_file(path):
+                if path in self._file_manager:
+                    data = self._file_manager[path].read()
+                self.close_file(path)
             return data
         except Exception as e:
-            print('ERROR!!! ', e)
+            print("ERROR!!! ", e)
             return None

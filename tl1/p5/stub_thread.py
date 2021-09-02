63e3f23efebeed1a9f5eb1a42a106a60ecdb0391
vicabitu <victor.ch370@gmail.com>
2021-09-02 11:39:02 -0300

    Add while loop in thread stub

--- tl1/p5/stub_thread.py
+++ tl1/p5/stub_thread.py
@@ -1,8 +1,6 @@
 import socket
 import threading
-import sys
 import pickle
-import time
 
 
 class FSStub(threading.Thread):
@@ -12,38 +10,39 @@ class FSStub(threading.Thread):
         self._adapter = file_system_adapter
 
     def run(self):
-        time.sleep(15)
-        data = self._channel.recv(4096)
-        if not data:
-            # TO DO: ver si es lo mejor el exit
-            sys.exit()
+        while True:
+            data = self._channel.recv(4096)
+            if not data:
+                break
 
-        payload = pickle.loads(data)
-        comando = payload.get("operacion", -1)
-        if comando == 1:
-            path = payload.get("path")
-            path_files = self._adapter.list_files(path)
-            response = {"paths": path_files}
-            response_serialized = pickle.dumps(response)
-            self._channel.sendall(response_serialized)
-        elif comando == 2:
-            path = payload.get("path")
-            open = self._adapter.open_file(path)
-            response = {"open": open}
-            response_serialized = pickle.dumps(response)
-            self._channel.sendall(response_serialized)
-        elif comando == 3:
-            path = payload.get("path")
-            data_file = self._adapter.read_file(path)
-            response = {"data_file": data_file}
-            response_serialized = pickle.dumps(response)
-            self._channel.sendall(response_serialized)
-        elif comando == 4:
-            path = payload.get("path")
-            close = self._adapter.close_file(path)
-            response = {"close": close}
-            response_serialized = pickle.dumps(response)
-            self._channel.sendall(response_serialized)
+            payload = pickle.loads(data)
+            comando = payload.get("operacion", -1)
+            if comando == 1:
+                path = payload.get("path")
+                path_files = self._adapter.list_files(path)
+                response = {"paths": path_files}
+                response_serialized = pickle.dumps(response)
+                self._channel.sendall(response_serialized)
+            elif comando == 2:
+                path = payload.get("path")
+                open = self._adapter.open_file(path)
+                response = {"open": open}
+                response_serialized = pickle.dumps(response)
+                self._channel.sendall(response_serialized)
+            elif comando == 3:
+                path = payload.get("path")
+                offset = payload.get("offset")
+                cant_bytes = payload.get("cant_bytes")
+                data_file = self._adapter.read_file(path, offset, cant_bytes)
+                response = {"data_file": data_file}
+                response_serialized = pickle.dumps(response)
+                self._channel.sendall(response_serialized)
+            elif comando == 4:
+                path = payload.get("path")
+                close = self._adapter.close_file(path)
+                response = {"close": close}
+                response_serialized = pickle.dumps(response)
+                self._channel.sendall(response_serialized)
 
 
 class Stub:
@@ -67,6 +66,7 @@ class Stub:
                     "Connected to: " + client_address[0] + ":" + str(client_address[1])
                 )
                 thread = FSStub(connection, self._adapter)
+                print(f"Driven by the thread: {thread.getName()}")
                 thread.start()
 
         except KeyboardInterrupt:

542bf5105811813b06cd06885ed32103b9dfb0d0
vicabitu <victor.ch370@gmail.com>
2021-09-02 11:35:44 -0300

    Adds top-level package directory

--- tl1/p3/servidor.py
+++ tl1/p3/servidor.py
@@ -1,11 +1,24 @@
+import sys
 from file_system import FS
 from server import Server
-from p3a import ServerStub
+
+# Adds top-level package directory.
+sys.path.append("..")
+
+# from p3a import ServerStub
+
+# from p3b import ServerStub
+
+from p5 import ServerStub
+
+# from p7 import ServerStub
+
 
 def main():
-    stub = ServerStub(FS, '50051')
+    stub = ServerStub(FS(), "50051")
     servidor = Server(stub)
     servidor.inicializar()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()

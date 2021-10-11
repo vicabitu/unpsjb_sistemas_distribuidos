d3f8f78b61d1e7057ca3634f68fbfbd1e6b4fd74
vicabitu <victor.ch370@gmail.com>
2021-10-11 17:31:05 -0300

    Add code comments tl2 p1

--- tl2/p1/client.py
+++ tl2/p1/client.py
@@ -6,16 +6,20 @@ from timeit import default_timer
 
 
 class ClockThread(threading.Thread):
+    """Clase que representa el reloj"""
+
     def __init__(self, remote_object, client_hour):
         self.time_server = remote_object
         self.client_hour = client_hour
 
     def convert_string_to_object_datetime(self, string_time):
+        """Metodo que se encarga de transformar un formato de fecha a un objeto datetime"""
         format = "%Y-%m-%d %H:%M:%S:%f"
         object_time = datetime.strptime(string_time, format)
         return object_time
 
     def get_smallest_round_trip_time(self, times):
+        """Metodo que se encarga de devolver el menor tiempo de ida y vuelta"""
         result = times[0]
         smaller = times[0].get("half_round_trip_time")
         for t in times:
@@ -25,18 +29,22 @@ class ClockThread(threading.Thread):
         return result
 
     def calculate_time_difference(self, server_hour):
+        """Metodo que se encarga de calcular la diferencia entre
+        la hora del servidor y la hora del cliente"""
         difference = server_hour - self.client_hour
         return difference.total_seconds()
 
     def process_difference(self, difference):
+        """Metodo que se encarga de determinar la deriva
+        correcta en base a la diferencia de horas"""
         if difference >= 60:
-            return -1  # me duermo 0
+            return -1
         elif difference >= 30:
-            return -0.8  # me duero 0.2
+            return -0.8
         elif difference >= 1:
-            return -0.6  # me duermo 0.4
+            return -0.6
         elif difference > -1 and difference < 1:
-            return 0  # me duermo 1
+            return 0
         # A partir de aca me tengo que retrasar, me duermo mas (Estoy adelantado)
         elif difference <= -60:
             return 12
@@ -48,27 +56,21 @@ class ClockThread(threading.Thread):
             return 10
 
     def process_drift(self, drift):
+        """Metodo que se encarga de dormir el reloj segun la deriva"""
         time.sleep(1 + drift)
 
     def cristian_algorithm(self):
+        """Metodo que se encarga de calcular el algoritmo de Cristian"""
 
         times = []
 
         for i in range(5):
             beginning = time.time()
-
             string_server_time = self.time_server.get_time()
-
             end = time.time()
-
             round_trip_time = end - beginning
-
             server_time = self.convert_string_to_object_datetime(string_server_time)
-
-            # print(f"Tiempo total de ida y vuelta [C(T4) - C(T1)]: {round_trip_time}")
-
             half_round_trip_time = round_trip_time / 2
-
             times.append(
                 {
                     "half_round_trip_time": half_round_trip_time,
@@ -76,26 +78,18 @@ class ClockThread(threading.Thread):
                 }
             )
 
-            # print(f"> Tiempo total de ida [C(T4) - C(T1)] / 2: {half_round_trip_time}")
-            # print(f"Hora del servidor (T3): {string_server_time}")
-            # print("---------------------------------------------------------------\n")
-
         shorter_half_round_trip_time = self.get_smallest_round_trip_time(times)
-
         server_time = shorter_half_round_trip_time.get("server_time")
         shorter_half_round_trip_time = shorter_half_round_trip_time.get(
             "half_round_trip_time"
         )
         exact_hour = server_time + timedelta(seconds=shorter_half_round_trip_time)
-
         return exact_hour
 
     def run(self):
 
         quantity = 0
 
-        # ayuda cuando el reloj esta lejos
-        # El numero representa las veces que tengo que iterar para llamar a cristian otra vez
         while True:
             start = default_timer()
             if quantity == 0:
@@ -120,13 +114,12 @@ def main():
     time_server = Pyro4.Proxy(
         "PYRONAME:my.home"
     )  # use name server object lookup uri shortcut
-    # aca tengo que instanciar una hora inicial hardcodeada, futura o pasada
     # test_date = datetime(2021, 9, 25)
     # test_date = datetime(2021, 9, 30, 19, 50, 0)
     # test_date = datetime(2021, 10, 7, 19, 54, 0)
-    test_date = datetime(2021, 10, 8, 15, 18, 0)
+    test_date = datetime(2021, 10, 11, 15, 18, 0)
     clock = ClockThread(time_server, test_date)
-    print(f"Hora del cliente {test_date}")
+    print(f"Hora del cliente: {test_date}")
     clock.run()
 
 
--- tl2/p1/server.py
+++ tl2/p1/server.py
@@ -14,6 +14,5 @@ daemon = Pyro4.Daemon()  # make a Pyro daemon
 ns = Pyro4.locateNS()  # find the name server
 uri = daemon.register(TimeServer)  # register the greeting maker as a Pyro object
 ns.register("my.home", uri)  # register the object with a name in the name server
-
 print("Ready.")
 daemon.requestLoop()  # start the event loop of the server to wait for calls
